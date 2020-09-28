c77681f77f0da3ba4ff0b5e0f67b826cd99ce480
s163962 <s163962@student.dtu.dk>
2020-09-28 15:31:46 +0200

    adding files from demos on 28/09

---
+++ catkin_ws/src/proj1/scripts/lecture_5_4_open_gripper.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from math import ceil
+from numpy import zeros, array, linspace
+from sensor_msgs.msg import JointState
+import shape_msgs.msg as shape_msgs
+import geometry_msgs.msg
+import moveit_msgs.msg
+import moveit_commander
+import tf_conversions
+import rospy
+import copy
+import sys
+import roslib
+roslib.load_manifest('proj1')
+
+
+currentJointState = JointState()
+
+
+def jointStatesCallback(msg):
+    global currentJointState
+    currentJointState = msg
+
+
+if __name__ == '__main__':
+    rospy.init_node('test_publish')
+
+    # Setup subscriber
+    #rospy.Subscriber("/joint_states", JointState, jointStatesCallback)
+
+    pub = rospy.Publisher("/jaco/joint_control", JointState, queue_size=1)
+
+    currentJointState = rospy.wait_for_message("/joint_states", JointState)
+    print 'Received!'
+    currentJointState.header.stamp = rospy.get_rostime()
+    tmp = 0.005
+    #tmp_tuple=tuple([tmp] + list(currentJointState.position[1:]))
+    currentJointState.position = tuple(
+        list(currentJointState.position[:6]) + [tmp] + [tmp] + [tmp])
+    rate = rospy.Rate(10)  # 10hz
+    for i in range(3):
+        pub.publish(currentJointState)
+        print 'Published!'
+        rate.sleep()
+
+    print 'end!'
