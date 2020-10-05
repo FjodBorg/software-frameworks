b98ee5598554260fbf34a60241208472de7ae709
Fjodor <s163903@student.dtu.dk>
2020-10-05 13:53:12 +0200

    made small change so less conflicts occur

--- catkin_ws/src/proj1/scripts/move_cubes_to_bucket.py
+++ catkin_ws/src/proj1/scripts/move_cubes_to_bucket.py
@@ -16,16 +16,16 @@ from gazebo_msgs.srv import GetWorldProperties
 def find_cube(models, model_coordinates, p, scene):
     # TODO fix example code
     
-    model_names = [i for i in models().model_names if "cube" in i]
+    #model_names = [i for i in models().model_names if "cube" in i]
     
-    height = 1.4
-    scene.world.collision_objects.clear()
-    for model_name in model_names:
-        rospy.loginfo(model_coordinates(model_name,"").pose.position)
-        p.header.frame_id = robot.get_planning_frame()
-        p.pose.position = model_coordinates(model_name,"").pose.position
-        p.pose.position.z = height/2
-        scene.add_box(model_name, p, (0.5, 0.5, height))
+    #height = 1.4
+    #scene.world.collision_objects.clear()
+    #for model_name in model_names:
+        #rospy.loginfo(model_coordinates(model_name,"").pose.position)
+        #p.header.frame_id = robot.get_planning_frame()
+        #p.pose.position = model_coordinates(model_name,"").pose.position
+        #p.pose.position.z = height/2
+        #scene.add_box(model_name, p, (0.5, 0.5, height))
         
     cube_pos = 0
     return cube_pos
@@ -34,22 +34,21 @@ def find_bucket(models, model_coordinates, p, scene):
 
     # TODO fix example code
 
-
-    model_names = [i for i in models().model_names if "bucket" in i]
+    #model_names = [i for i in models().model_names if "bucket" in i]
     
-    height = 1.4
-    scene.world.collision_objects.clear()
-    for model_name in model_names:
-        rospy.loginfo(model_coordinates(model_name,"").pose.position)
-        p.header.frame_id = robot.get_planning_frame()
-        p.pose.position = model_coordinates(model_name,"").pose.position
-        p.pose.position.z = height/2
-        scene.add_box(model_name, p, (0.5, 0.5, height))
+    #height = 1.4
+    #scene.world.collision_objects.clear()
+    #for model_name in model_names:
+        #rospy.loginfo(model_coordinates(model_name,"").pose.position)
+        #p.header.frame_id = robot.get_planning_frame()
+        #p.pose.position = model_coordinates(model_name,"").pose.position
+        #p.pose.position.z = height/2
+        #scene.add_box(model_name, p, (0.5, 0.5, height))
 
     bucket_pos = 0
     return bucket_pos
 
-def move_path():
+def move_path(models, model_coordinates, p, scene, group, robot):
     return 0
 
 if __name__ == "__main__":
@@ -62,6 +61,8 @@ if __name__ == "__main__":
     scene = moveit_commander.PlanningSceneInterface()
     p = geometry_msgs.msg.PoseStamped()
     
-
+    find_cube(models, model_coordinates, p, scene)
+    find_bucket(models, model_coordinates, p, scene)
+    move_path(models, model_coordinates, p, scene, group, robot)
 
     
